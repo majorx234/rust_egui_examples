a10798e0f47a5206bd3228aa5b4d334182187fe7
Majorx234 <majorx234@googlemail.com>
2023-01-21 18:31:39 +0100

    Add skeleton of painter example

---
+++ src/painter/bin/main.rs
@@ -0,0 +1,109 @@
+use cli_lib::read_data;
+use eframe::egui;
+use eframe::egui::plot::{Value, Values};
+use eframe::egui::{emath, Color32, Painter, Pos2, Rect, Sense, Stroke, Ui};
+
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(default))]
+pub struct Painting {
+    /// in 0-1 normalized coordinates
+    lines: Vec<Vec<Pos2>>,
+    stroke: Stroke,
+}
+
+impl Default for Painting {
+    fn default() -> Self {
+        Self {
+            lines: Default::default(),
+            stroke: Stroke::new(1.0, Color32::from_rgb(25, 200, 100)),
+        }
+    }
+}
+
+impl Painting {
+    pub fn ui_control(&mut self, ui: &mut egui::Ui) -> egui::Response {
+        ui.horizontal(|ui| {
+            egui::stroke_ui(ui, &mut self.stroke, "Stroke");
+            ui.separator();
+            if ui.button("Clear Painting").clicked() {
+                self.lines.clear();
+            }
+        })
+        .response
+    }
+
+    pub fn ui_content(&mut self, ui: &mut Ui) -> egui::Response {
+        let (mut response, painter) =
+            ui.allocate_painter(ui.available_size_before_wrap(), Sense::drag());
+
+        let to_screen = emath::RectTransform::from_to(
+            Rect::from_min_size(Pos2::ZERO, response.rect.square_proportions()),
+            response.rect,
+        );
+        let from_screen = to_screen.inverse();
+
+        if self.lines.is_empty() {
+            self.lines.push(vec![]);
+        }
+
+        let current_line = self.lines.last_mut().unwrap();
+
+        if let Some(pointer_pos) = response.interact_pointer_pos() {
+            let canvas_pos = from_screen * pointer_pos;
+            if current_line.last() != Some(&canvas_pos) {
+                current_line.push(canvas_pos);
+                response.mark_changed();
+            }
+        } else if !current_line.is_empty() {
+            self.lines.push(vec![]);
+            response.mark_changed();
+        }
+
+        let shapes = self
+            .lines
+            .iter()
+            .filter(|line| line.len() >= 2)
+            .map(|line| {
+                let points: Vec<Pos2> = line.iter().map(|p| to_screen * *p).collect();
+                egui::Shape::line(points, self.stroke)
+            })
+            .collect();
+
+        painter.extend(shapes);
+
+        response
+    }
+}
+
+struct PaintingApp {
+    size: usize,
+}
+
+impl Default for PaintingApp {
+    fn default() -> Self {
+        Self { size: 0 }
+    }
+}
+
+impl eframe::App for PaintingApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Painter");
+            /* ui.horizontal(|ui| {
+                Painter::new("my_painter")
+                    .view_aspect(2.0)
+                    .show(ui, |painter_ui| painter_ui.show());
+            })*/
+        });
+    }
+}
+
+fn main() {
+    let painting_app = PaintingApp { size: 0 };
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Painting App",
+        options,
+        Box::new(|_cc| Box::new(painting_app)),
+    );
+}
