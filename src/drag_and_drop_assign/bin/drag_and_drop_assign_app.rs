d42083d73dd4de13d99a66fd9a5436044af5135a
MajorX234 <majorx234@googlemail.com>
2023-11-19 20:48:04 +0100

    Dived ui in 3 columns

--- src/drag_and_drop_assign/bin/drag_and_drop_assign_app.rs
+++ src/drag_and_drop_assign/bin/drag_and_drop_assign_app.rs
@@ -20,6 +20,11 @@ impl eframe::App for DragAndDropAssignApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("DragAndDropAssignApp");
+            ui.columns(3, |columns| {
+                columns[0].label("base_elements");
+                columns[1].label("assigned_elements");
+                columns[2].label("not_assigned_elements");
+            })
         });
     }
 }
