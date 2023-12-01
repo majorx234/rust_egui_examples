4de57041e24e192d14878d3c04ff24611e6d1b40
MajorX234 <majorx234@googlemail.com>
2023-12-01 23:34:01 +0100

    Add implementation to paint status indicator

--- src/bin/status_elements_test_app/elements.rs
+++ src/bin/status_elements_test_app/elements.rs
@@ -4,7 +4,42 @@ use epaint::Stroke;
 fn status_indicator_ui(ui: &mut egui::Ui, status: &bool) -> egui::Response {
     let desired_size = ui.spacing().interact_size.y * egui::vec2(2.0, 1.0);
     let (rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
+
+    let width = 8.0;
+    let height = 1.0;
+    let desired_size = ui.spacing().interact_size.y * egui::vec2(width, height);
+    let (rect, response) = ui.allocate_exact_size(desired_size, egui::Sense::hover());
     //TODO implement painter
+    if ui.is_rect_visible(response.rect) {
+        let visuals = ui.style().visuals.clone();
+        let rounding = rect.height() / 2.0;
+        ui.painter()
+            .rect(rect, rounding, visuals.extreme_bg_color, Stroke::NONE);
+        let inner_rect =
+            egui::Rect::from_min_size(rect.min, egui::vec2(rect.width(), rect.height()));
+        let (dark, bright) = (0.7, 1.0);
+        if *status {
+            let color_factor = bright;
+            ui.painter().rect(
+                inner_rect,
+                rounding,
+                egui::Color32::from(
+                    egui::Rgba::from(visuals.selection.bg_fill) * color_factor as f32,
+                ),
+                Stroke::NONE,
+            );
+        }
+        let status_str = if *status { "on" } else { "off" };
+        let text: egui::WidgetText = format!("{}", status_str).into();
+        let galley = text.into_galley(ui, Some(false), f32::INFINITY, egui::TextStyle::Button);
+        let text_pos = rect.left_center()
+            - egui::Vec2::new(galley.size().x / 2.0, galley.size().y / 2.0)
+            + egui::vec2(rect.width() / 2.0, 0.0);
+        let text_color = visuals
+            .override_text_color
+            .unwrap_or(visuals.selection.stroke.color);
+        galley.paint_with_fallback_color(&ui.painter().with_clip_rect(rect), text_pos, text_color);
+    }
     response
 }
 
